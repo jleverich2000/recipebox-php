c238baaa7b51068496abadf4ecc30133cd9a0906
Jolleen Leverich <jleverich2000@gmail.com>
2018-04-21 20:01:22 -0400

    added basic controller twig and repo

---
+++ src/Controller/RecipeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
+
+class RecipeController extends Controller
+{
+    /**
+     * @Route("/recipe", name="recipe")
+     */
+    public function index()
+    {
+        $recipeId = 0;
+
+
+        $recipes = $this->getDoctrine()
+        ->getRepository(Recipe::class)
+        ->findAll();
+
+        return $this->render('recipe/recipe.twig', [
+            'recipe_name' => $recipes[$recipeId]['title'] ,
+            'recipe_ingredients' => json_decode($recipes[$recipeId]['ingredients']),
+            'recipe_steps' => $recipes[$recipeId]['steps'] ,
+            'recipe_method' => $recipes[$recipeId]['method'] ,
+
+        ]);
+    }
+ /**
+     * @Route("/recipe", name="recipe")
+    //  */
+    // public function index()
+    // {
+    //       // you can fetch the EntityManager via $this->getDoctrine()
+    //     // or you can add an argument to your action: index(EntityManagerInterface $entityManager
+    //     $entityManager = $this->getDoctrine()->getManager();
+
+    //     $recipe = new Recipe();
+    //     $recipe->setTitle('Keyboard');
+    //     $recipe->setIngredients('Keyboard');
+    //     $recipe->setSteps('Keyboard');
+    //     $recipe->setPrepTime(30);
+    //     $recipe->setMethod('Keyboard');
+    //     $recipe->setCatagory('Keyboard');
+    //     $recipe->setMeal('Keyboard');
+    //     $recipe->setSeason('Keyboard');
+    //     $recipe->setActive(1);
+
+    //     // tell Doctrine you want to (eventually) save the Product (no queries yet)
+    //     $entityManager->persist($recipe);
+       
+    //     // actually executes the queries (i.e. the INSERT query)
+    //     $entityManager->flush();
+
+    //     return $this->render('recipe/recipe.twig', [
+    //         'controller_name' => 'RecipeController',
+    //     ]);
+    // }
+    /**
+ * @Route("/recipe/{id}", name="recipe_show")
+ */
+public function showAction($id)
+{
+    $recipe = $this->getDoctrine()
+        ->getRepository(Recipe::class)
+        ->find($id);
+
+    if (!$recipe) {
+        throw $this->createNotFoundException(
+            'No recipe found for id '.$id
+        );
+    }
+
+    return $this->render('recipe/recipe.twig', [
+        'Title' =>  $recipe->getTitle,
+    ]);
+
+    // or render a template
+    // in the template, print things with {{ product.name }}
+    // return $this->render('product/show.html.twig', ['product' => $product]);
+}
+}
---
+++ src/Repository/RecipeRepository.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Recipe;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+/**
+ * @method Recipe|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Recipe|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Recipe[]    findAll()
+ * @method Recipe[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class RecipeRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, Recipe::class);
+    }
+
+    function save($table, $insData ) 
+    {
+        $columns = implode(", ",array_keys($insData));
+        $values  = array_values($insData);
+        $valuesString =  sqlStringBuilder($values);
+        $sql = "INSERT INTO $table ($columns) VALUES($valuesString)";
+        queryMysql($sql);
+    }
+      
+    public function findAllLessThanPrepTime($prepTime): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+    
+        $sql = '
+            SELECT * FROM recipe r
+            WHERE r.prepTime < :prepTime
+            ORDER BY r.prepTime ASC
+            ';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['prepTime' => 100]);
+    
+        // returns an array of arrays (i.e. a raw data set)
+        return $stmt->fetchAll();
+    }
+
+    public function findAllByCatagory($catagory): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+    
+        $sql = '
+            SELECT * FROM recipe r
+            WHERE r.catagory = :catagory
+            ORDER BY r.title ASC
+            ';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['recipes' => 100]);
+    
+        // returns an array of arrays (i.e. a raw data set)
+        return $stmt->fetchAll();
+    }
+
+    public function findAllByMethod($method): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+    
+        $sql = '
+            SELECT * FROM recipe r
+            WHERE r.method = :method
+            ORDER BY r.title ASC
+            ';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['recipes' => 100]);
+    
+        // returns an array of arrays (i.e. a raw data set)
+        return $stmt->fetchAll();
+    }
+
+    public function findAll(): array
+    {
+        $conn = $this->getEntityManager()->getConnection();
+    
+        $sql = '
+            SELECT * FROM recipe r
+            ORDER BY r.title ASC
+            ';
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(['recipes' => 100]);
+    
+        // returns an array of arrays (i.e. a raw data set)
+        return $stmt->fetchAll();
+    }
+//     function update($table, $setProperty, $setValue, $queryProperty, $queryValue)
+//     {
+//         $sql = "UPDATE $table SET  $setProperty='$setValue' WHERE $queryProperty = '$queryValue'";
+//         return queryMysql($sql);
+// }
+}
